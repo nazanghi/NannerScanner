62787bff674112d638c4a848e1bc3571f0b3447f
Nicola Zanghi <nico.zang7@gmail.com>
2020-10-21 11:41:08 -0400

    Adds banana and ISS trivia. Cleans Code and improves trivia button functionality

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ if ('geolocation' in navigator) {
 
 const ISS_LOC = "http://api.open-notify.org/iss-now.json" //API for the ISS
 const GMAP_KEY = "AIzaSyCIhMaab2I5DZf5GaWnhVxZL2He90U-wfA" //API Key for Google Maps
-const startingAddress = document.getElementById("starting-address") //where the user types their address
+const startingAddress = document.getElementById("startingAddress") //where the user types their address
 let bsAddress = "245 Front Street, Key West, FL 33040" //this will later be a math random at an array of bs addresses
 let addressInput = startingAddress.value || bsAddress //gives user address OR if null, Margaritaville, etc
 const GMAP_URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${addressInput}&key=${GMAP_KEY}`
@@ -187,19 +187,31 @@ userForm.addEventListener("submit", async (e) => {     //proof that anonymous fu
 
 
 let bananaFacts = [
-    "bananafact1", "bananafact2", "bananafact3", "bananafact4", "bananafact5", "bananafact6"
+    "Today's common banana, the Cavendish, is different from the Gros Michael from before the 60s, as Gros Michael Bananas were wiped out by The Panama Disease. If you're curious what Gros Michael bananas tasted like, try a Banana-flavored Laffy Taffy or Runt! Its recipe is unchanged and based on the bananas of its time.",  
+ "The inside of a banana peel can help relieve itching and inflammation, like from bug bites or poison ivy!", 
+ "Bananas are considered a berry and the banana tree is actually an herb!", 
+ "India consumes the most bananas in the world. In 2017, they ate more than the next three countries COMBINED.", 
+ "There are more than 1,000 kinds of bananas, including Mona Lisa, Ice Cream, Goldfinger, and the Blue Java.", 
+ "Rubbing a banana peel on a scratched CD will help fill the scratches without damaging the plastic finish!",
+ "The slipping on a banana peel gag originates in the 1850s, when discarded peels would reach a gooey state after days or weeks of rotting."
 ]
 
 
-let issFacts = ["spacefact1", "spacefact2", ]
+let issFacts = [
+    "The International Space Station has been continuously operated since November 2000",
+ "The ISS moves five miles per second, and orbits the Earth every 90 minutes. That means every 24 hours, it experiences 16 sunrises and sunsets!",
+"The internal pressurized volume of the ISS is 916 cubic meters. That means it could hold 7,328,000 bananas, but that would probably be a bad idea.",
+ "The ISS uses about 1.5 MILLION lines of computer code. Its support on Earth uses an additional 3 million!",
+]
 
 let bananaFactButton = document.getElementById("banana-facts")
 
 
 const theBananaFactor = () => {
-    document.querySelector("#banana-facts").textContent= `${
+    document.querySelector("#bananaFacts").textContent= `${
         bananaFacts[Math.floor(Math.random()*bananaFacts.length)]}`
+    
     }    
     
-bananaFactButton.addEventListener('click', theBananaFactor)
+bananaFactButton.addEventListener('click', theBananaFactor())
     
