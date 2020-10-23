06acdcfe46c6b456a110a6edcefa42bfcd773ce9
Nicola Zanghi <nico.zang7@gmail.com>
2020-10-23 08:01:16 -0400

    edits README

--- app.js
+++ app.js
@@ -1,84 +1,3 @@
-/**
-FROM THE TOP, DIRECTIONS
-
-
-
-1. User inputs an address
-2. Clicks Submit
-3. NannerScanner takes the ISS current location in Long/Lat
-4. And passes it as an argument for The NannerScannerJammer
-5. Google Maps takes the user's address and turns it into Long/Lat
-6. NannerScanner takes the Google Maps information and passes it as an argument for...
-7. The NannerScannerJammer (NSJ)
-8. NSJ takes both ISS & user Long & Lat as arguments for formula to calculate distance in KM
-9. returned distance in KM is then turned into CM and divided by banana
-10. User receives result of how many bananas appended to a div in HTML
-11. Replacing results with new results as the NSJ is invoked
-
-
-PROGRAMMING STEPS
-    ☑︎1. Create necessary elements in HTML
-    2. Create functions that will access API
-    ☑︎3. Create Formula (NSJ) for calculating distance (and bananas)
-    4. Access the long/lat values of respective maps
-    5. Pass on long/lat values into NSJ and return results (in KM and Banana)
-    6. Display results in km and banana for user
-    7. Reset values when NSJ is invoked again
-    8. Style CSS to look like an old terminal
-
-PROJECT REQUIREMENTS
-1. ReadMe Need to make 
-2. Utilize Flexbox/grid Need to do more 
-3. Secondary HTML page Could Use Improvement
-4. two event handlers DONE
-5. 20 git commits
-6. Clean Code
-
-HANGUPS:
-
-Second HTML: Need to have an additional HTML page
-
-
-STRETCH:
-More stupid addresses
-More stupid CSS themes
-
-
-/**STRETCH GOALS
- * 
- * 
-Could be fun to just give them a random location instead of letting them pick a new one
--Make an array of stupid default locations
--Start with Margaritaville, when MVP is met, make array to store BS addresses
--like Margaritaville in Florida
--or the last blockbuster, in Bend, Oregon
--Alamogordo, New Mexico mass burial of ET games
--world's largest banana farm
--world's largest banana
--bluth banana stand location
--where bananas in pajamas was filmed
--where the factory that makes bananagrams is
--curious george theme park
--Cocomo, Indiana John Stamos played Steel Drums
-
- */
-
-
-// import spinninNanner from "./img/spinninNanner"
-// console.log(spinninNanner)
-console.log("://NANNER_SCANNER_TESTING")
-
-if ('geolocation' in navigator) {
-    console.log ('://NANNER_SCANNER_GEOLOCATION_UPLINK_ONLINE')
-} else {
-    console.log('yo ur browser suxxxx')
-    alert("Sorry, but your browser's incompatible with the Nanner Scanner.")
-}
-
-
-
-
-
 const ISS_LOC = "http://api.open-notify.org/iss-now.json" //API for the ISS
 const GMAP_KEY = "AIzaSyCIhMaab2I5DZf5GaWnhVxZL2He90U-wfA" //API Key for Google Maps
 const startingAddress = document.getElementById("startingAddress") //where the user types their address
@@ -120,13 +39,13 @@ userForm.addEventListener("submit", async (e) => {
     
     console.log("://BANANA_BUTTON_VALUE_RECEIVED");
     let distanceKM = parseInt((Math.sqrt( ((issLongitude - userLongitude)**2) + ((issLatitude - userLatitude)**2)))*111);
-    console.log(`Coordinates:`, issLongitude, userLongitude, issLatitude, userLatitude)
+    console.log(`Coordinates: ISSLong: ${issLongitude} UserLong: ${userLongitude} issLat: ${issLatitude} userLat: ${userLatitude}`)
 
     let bananaDistance = parseInt(distanceKM/(banana/kM))
     
     
     
-    bananaResults.innerText = `Your location is about ${bananaDistance} bananas away from the International Space Station's current location.`
+    bananaResults.innerText = `Your location is about ${bananaDistance} bananas away from the International Space Station's current GPS coordinates.`
     
 })
 
@@ -152,5 +71,5 @@ document.addEventListener("DOMContentLoaded", () =>{
             (setTimeout(nannerfy,(2000*i)))
         }
     }
-    setTimeout(nannerSpawn, 17000)
+    setTimeout(nannerSpawn, 4000)
 })
